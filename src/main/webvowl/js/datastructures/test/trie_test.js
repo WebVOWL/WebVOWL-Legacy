3a26c9c8f22d2d72c5c25ecd1dce07103f2f8564
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2025-05-02 15:34:05 +0200

    Add additional test cases

--- src/main/webvowl/js/datastructures/test/trie_test.js
+++ src/main/webvowl/js/datastructures/test/trie_test.js
@@ -1,5 +1,43 @@
 import Trie from "../trie"
 
+/**
+ * @param {Trie} trie
+ */
+function traverseTrie(trie) {
+    let stack = [trie.base]
+    let output = []
+    while (stack.length) {
+        for (const node of stack.shift().children.values()) {
+            stack.push(node)
+            if (node.end) {
+                output.push(
+                    node.getWord() + ` [${Array.from(node.data.values())}]`,
+                )
+            }
+        }
+    }
+    return output.join(" ~ ")
+}
+
+/**
+ * @param {Trie} trie
+ * @param {string} word
+ * @param {number} [limit]
+ */
+function find(trie, word, limit) {
+    const searchMatches = trie.find(word, limit)
+    let output = []
+    for (let i = 0; i < searchMatches.length; i++) {
+        let str = searchMatches[i][0]
+        /**
+         * @type {Set<string>}
+         */
+        str += ` [${Array.from(searchMatches[i][1].values())}]`
+        output.push(str)
+    }
+    return output.join(" ~ ")
+}
+
 export default function testTrie() {
     let trie = new Trie()
     let testData = ["Rem", "Ram", "Fubuki", "Unicorn"]
@@ -14,16 +52,20 @@ export default function testTrie() {
         console.log(`[${i}]: ${label}`)
     }
     console.log("--- Performing membership tests ---")
-    console.log(`R | ${trie.find("R").join(" ~ ")}`)
-    console.log(`Re | ${trie.find("Re").join(" ~ ")}`)
-    console.log(`Rem | ${trie.find("Rem").join(" ~ ")}`)
-    console.log(`r | ${trie.find("r").join(" ~ ")}`)
-    console.log(`Fub | ${trie.find("Fub").join(" ~ ")}`)
-    console.log(`corn | ${trie.find("corn").join(" ~ ")}`)
-    console.log(`"" (1 word) | ${trie.find("", 1).join(" ~ ")}`)
+    console.log(`Find 'R' | ${find(trie, "R")}`)
+    console.log(`Find 'Re' | ${find(trie, "Re")}`)
+    console.log(`Find 'Rem' | ${find(trie, "Rem")}`)
+    console.log(`Find 'r' | ${find(trie, "r")}`)
+    console.log(`Find 'Fub' | ${find(trie, "Fub")}`)
+    console.log(`Find 'corn' | ${find(trie, "corn")}`)
+    console.log(`Find '""' (1 word limit) | ${find(trie, "", 1)}`)
     console.log(`Contains 'Fubuki' | ${trie.contains("Fubuki")}`)
     console.log(`Contains 'R' | ${trie.contains("R")}`)
     console.log(`Contains 'Some' | ${trie.contains("Some")}`)
+    console.log(`Remove 'Unicorn' | Returns ${trie.remove("Unicorn")}`)
+    console.log(`Find 'Unicorn' | ${find(trie, "Unicorn")}`)
+
+    console.log(`Result: ${traverseTrie(trie)}`)
 
     // Test duplicate values
     console.log("--- Adding duplicate values to trie ---")
@@ -34,16 +76,22 @@ export default function testTrie() {
         console.log(`[${offset}]: ${label}`)
     }
     console.log("Performing duplicate membership tests")
-    console.log(`R | ${trie.find("R").join(" ~ ")}`)
-    console.log(`Re | ${trie.find("Re").join(" ~ ")}`)
-    console.log(`Rem | ${trie.find("Rem").join(" ~ ")}`)
-    console.log(`r | ${trie.find("r").join(" ~ ")}`)
-    console.log(`Fub | ${trie.find("Fub").join(" ~ ")}`)
-    console.log(`corn | ${trie.find("corn").join(" ~ ")}`)
-    console.log(`"" (all words) | ${trie.find("").join(" ~ ")}`)
+    console.log(`Find 'R' | ${find(trie, "R")}`)
+    console.log(`Find 'Re' | ${find(trie, "Re")}`)
+    console.log(`Find 'Rem' | ${find(trie, "Rem")}`)
+    console.log(`Find 'r' | ${find(trie, "r")}`)
+    console.log(`Find 'Fub' | ${find(trie, "Fub")}`)
+    console.log(`Find 'corn' | ${find(trie, "corn")}`)
+    console.log(`Find '""' (no limit) | ${find(trie, "")}`)
     console.log(`Contains 'Fubuki' | ${trie.contains("Fubuki")}`)
     console.log(`Contains 'R' | ${trie.contains("R")}`)
     console.log(`Contains 'Some' | ${trie.contains("Some")}`)
+    console.log(`Remove 'Fubuki' [2] | Returns ${trie.remove("Fubuki", 2)}`)
+    console.log(`Find 'Fubuki' | ${find(trie, "Fubuki")}`)
+    console.log(`Remove 'Rem' | Returns ${trie.remove("Rem")}`)
+    console.log(`Find 'Ram' | ${find(trie, "Ram")}`)
+
+    console.log(`Result: ${traverseTrie(trie)}`)
 
     console.log("=== Testing of Trie finished ===")
 }
